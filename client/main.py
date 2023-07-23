9de78b79250685a6fa1ee75f48ad902573875707
Ashish Karhade <akarhade5@gmail.com>
2023-07-23 16:13:59 +0530

    grpc connection tested for 1 func

--- client/main.py
+++ client/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+import logging
+import grpc
+import users_pb2
+import users_pb2_grpc
 
+app = FastAPI()
 
 @app.get("/")
 async def root():
@@ -11,3 +15,16 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+def run():
+    response = []
+    print("Will try to get the user ...")
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = users_pb2_grpc.UserServiceStub(channel)
+        response = stub.GetUsers(users_pb2.GetUsersRequest())
+    print(response.user)
+
+
+if __name__ == "__main__":
+    run()
--- server/main.py
+++ server/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from concurrent import futures
+import logging
+import grpc
+import users_pb2
+import users_pb2_grpc
 
 app = FastAPI()
 
@@ -11,3 +16,29 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+class Users(users_pb2_grpc.UserService):
+    def GetUsers(self, request, context):
+        return users_pb2.GetUsersResponse(user=[
+            users_pb2.User(
+                id="1",
+                name="Ashish K",
+                email="ashish@gmail.com",
+                password="password"
+            )
+        ])
+
+
+def serve():
+    port = "50051"
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    users_pb2_grpc.add_UserServiceServicer_to_server(Users(), server)
+    server.add_insecure_port("[::]:" + port)
+    server.start()
+    print("Server started, listening on " + port)
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
